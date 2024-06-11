20cb197fc10f9b0e0511a34af7590af523c26554
rida iquen <r.aitiquen@gmail.com>
2024-06-11 23:03:26 +0200

    fix contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ function Contact(props) {
     };
 
     try {
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
